8e32a7cb39cdd59ceaf75b8d6097299f03be841b
TPhongWork <ttfongwork@gmail.com>
2024-12-31 19:58:58 +0700

    Auto-generated commit

--- assets/data/brand/brand-data.ts
+++ assets/data/brand/brand-data.ts
@@ -2,11 +2,11 @@ import { Brand } from "./brand-interface";
 
 const BrandData :  Brand ={
     imgUrl: [
-        "assets/img/pages/products/dia-1.jpg",
-        "assets/img/pages/products/dia-go.jpg",
+        "/assets/img/pages/products/dia-1.jpg",
+        "/assets/img/pages/products/dia-go.jpg",
         "assets/img/pages/products/thia-go.jpg",
-        "assets/img/pages/products/sanpham.jpg",
-        "assets/img/pages/products/dia-1.jpg",
+        "/assets/img/pages/products/sanpham.jpg",
+        "/assets/img/pages/products/dia-1.jpg",
         "assets/img/pages/products/dia-go.jpg",
     ]
 }
